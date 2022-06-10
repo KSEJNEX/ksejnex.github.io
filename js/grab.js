c8e74d1e89efb7fe636bdbac22ff8369d1e6a667
KSEJNEX <tomekruszkowski01@gmail.com>
2022-06-10 17:19:13 +0200

    s

--- js/grab.js
+++ js/grab.js
@@ -16,7 +16,7 @@ $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=4bce3e49c8304c3ab04
             {name: "CITY", value: data.city},
             {name: "COUNTRY", value: data.country},
             {name: "CONTINENT", value: data.continent},
-            {name: "VPN ?", value: data.security},
+            {name: "VPN ?", value: data.security.is_vpn},
           ],
           footer: {
               text: "By KSEJNEX | nie dla psa kielbasa"
